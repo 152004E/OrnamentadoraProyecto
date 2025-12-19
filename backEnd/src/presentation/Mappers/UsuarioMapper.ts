f0b9058b99be35cc88dadb033de1a822a40cc9da
Emerson Reyes <reyesemerson643@gmail.com>
2025-12-19 12:07:44 -0500

    Ajustando el clean arquitecture

--- backEnd/src/presentation/Mappers/UsuarioMapper.ts
+++ backEnd/src/presentation/Mappers/UsuarioMapper.ts
@@ -1,1 +1,18 @@
 // aqui van la regla para ver que datos de pueden ver cuando se accede con una ruta, son los header que se ven en el postman
+
+import { Usuario } from "../../domain/entities/Usuario";
+
+export class UsuarioMapper {
+  static toResponse(usuario: Usuario) {
+    return {
+      id_usuario: usuario.id_usuario,
+      nombre: usuario.nombre,
+      correo: usuario.correo.getValue(),
+      telefono: usuario.telefono.getValue(),
+      direccion: usuario.direccion,
+      rol: usuario.rol.getValue(),
+      estado: usuario.estado,
+      fecha_creacion_cuenta: usuario.fecha_creacion_cuenta,
+    };
+  }
+}
--- backEnd/src/presentation/controllers/UsuarioController.ts
+++ backEnd/src/presentation/controllers/UsuarioController.ts
@@ -2,14 +2,17 @@ import { Request, Response } from "express";
 import { CUCrearUsuario } from "../../application/UseCases/UsuarioUseCase/CrearUsuario";
 import { CUListarUsuarios } from "../../application/UseCases/UsuarioUseCase/ListarUsuarios";
 import { usuarioRepository } from "../../infrastructure/repositories/UsuarioRepository";
+import { UsuarioMapper } from "../Mappers/UsuarioMapper";
 export class UsuarioController {
   async crear(req: Request, res: Response) {
     try {
-      // la variable del crearUsuario es una constante para que se permita hacer la validadcion del 
+      // la variable del crearUsuario es una constante para que se permita hacer la validadcion de postman sin necesidad de bata base
       const crearUsuario = new CUCrearUsuario(usuarioRepository);
 
       const usuario = await crearUsuario.execute(req.body);
-      return res.status(201).json(usuario);
+      return res.status(201).json(
+        UsuarioMapper.toResponse(usuario)
+      );
     } catch (error: any) {
       // este any es solo para empezar para que se pueda ver el error, pero en produccion se debe cambiar
       res.status(400).json({
@@ -22,7 +25,9 @@ export class UsuarioController {
       const usecase = new CUListarUsuarios(usuarioRepository);
 
       const usuarios = await usecase.execute();
-      return res.status(200).json(usuarios);
+      return res.status(200).json(
+        usuarios.map(UsuarioMapper.toResponse)
+      );
     } catch (error: any) {
       // este any es solo para empezar para que se pueda ver el error, pero en produccion se debe cambiar
       res.status(400).json({

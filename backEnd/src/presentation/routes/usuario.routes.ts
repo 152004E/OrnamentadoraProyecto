577f952ee2eb90c4ee3c72d443a754b0dd5936fb
Emerson Reyes <reyesemerson643@gmail.com>
2026-01-03 22:51:25 -0500

    Jwt

--- backEnd/src/presentation/routes/usuario.routes.ts
+++ backEnd/src/presentation/routes/usuario.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { UsuarioController } from "../controllers/UsuarioController";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router = Router();
 const usuarioController = new UsuarioController();
 
-router.post("/usuarios" , usuarioController.crear)
-router.get("/usuarios" , usuarioController.listar)
-router.get("/usuarios/:id" , usuarioController.buscarPorId)
-router.put("/usuarios/:id" , usuarioController.actualizar)
-router.delete("/usuarios/:id", usuarioController.eliminar)
+router.post("/usuarios" , usuarioController.crear) // publico 
+router.use(authMiddleware); // 👈 desde aquí todo protegido
+
+router.get("/usuarios", usuarioController.listar);
+router.get("/usuarios/:id", usuarioController.buscarPorId);
+router.put("/usuarios/:id", usuarioController.actualizar);
+router.delete("/usuarios/:id", usuarioController.eliminar);
 
 export default router;  
--- backEnd/src/server.ts
+++ backEnd/src/server.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import usuarioRoutes from "./presentation/routes/usuario.routes";
 import authRoutes from "./presentation/routes/auth.routes";
 
+
+// configuracion para el JWT
+dotenv.config();
 const app = express();
 
 app.use(cors());
